509952ce4a7f8bd0840c8b63d1f051437bec4339
Stella M Thomas <stellamthomas1997@gmail.com>
2020-07-19 14:06:14 +0530

    commit2
    Login with Captcha

---
+++ mainbody.php
@@ -0,0 +1,46 @@
+
+		<!-- Notification start -->
+        <p style="margin-left: 10px;margin-right: 10px; font-size: 20px;text-align: center;font-family: 'Bebas Neue', cursive;">Notification</p><br>
+			<p style="margin-left: 10px;margin-right: 10px; font-size: 18px;font-family: 'Bebas Neue', cursive;">
+					<marquee bgcolor="#e6ebc3" style="margin-left: 150px;margin-right: 100px;"direction="left" scrollamount="7" width="80%" >Last date for paying bills is 20 July 2020</marquee>
+			</p>
+
+		<!-- Notification end -->	
+		
+		<!-- tv box start -->
+			<section class="banner_start">
+				<div class="inner_wrap1">
+					<div class="ptnr"  >
+						<div id="slider">
+							<ul>
+								<li><img src="images/1dgr_01.png" alt=" " /></li>
+							</ul>
+						</div>
+					</div>
+					<div class="small_tv"> </div>
+				</div>
+			</section>
+		<!-- tv box end -->
+
+		<!-- banner start -->
+			<section class="bootom_slider">
+				<div class="inner_wrap">
+					<div class="oc_slider">
+						<div class="oc_inner">
+							<div id="slider1">
+								<ul>
+									<li>
+										<a href="https://mygov.in/" target="_blank" class="external_link"><img src="images/my_gov.png" /></a>
+										<a href="https://india.gov.in/" target="_blank" class="external_link"><img src="images/india_gov.in.png" /></a>
+										<a href="http://powermin.nic.in/" target="_blank" class="external_link"> <img src="images/power.png"  /></a>
+										<a href="http://pmindia.gov.in/" target="_blank" class="external_link"> <img src="images/pm.png"  /></a>
+										<a href="https://swachhbharat.mygov.in/" target="_blank" class="external_link"> <img src="images/swach.png"  /></a>
+										<a href="http://www.nic.in/" target="_blank" class="external_link"> <img src="images/nic.png"  /></a>
+										<a href="http://cea.nic.in/" target="_blank" class="external_link"> <img src="images/ECE.png" /></a> 
+									</li>
+								</ul>
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>
---
+++ mainfooter.php
@@ -0,0 +1,21 @@
+
+			<div class="counter01">
+				<p>Visit Counter: <span id="counter" class="numcu" >139804426</span> (Since Today)</p>
+
+			</div>
+		<!-- banner end -->
+
+	<!-- footer section start -->
+		<footer>
+			<div class="inner_wrap">	</div>
+			<h4>Content owned, updated and maintained by Kerala Electricity Board, Ministry of Power,Government of Kerala.<br> KSEBLive is designed and developed by Stella M Thomas 2020 &copy;</h4>
+		</footer>
+	<!-- footer section end -->
+		<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>	
+	</body>
+</html>
+
+    
+    
