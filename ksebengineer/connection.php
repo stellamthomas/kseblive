bec72be62b09e6f532d6b30523d9eb773e0a906a
Stella M Thomas <stellamthomas2021@mca.ajce.in>
2021-06-02 09:12:25 -0700

    Update connection.php

--- ksebengineer/connection.php
+++ ksebengineer/connection.php
@@ -1,3 +1,4 @@
 <?php
-  $conn=mysqli_connect('localhost','root','','kseblive') or die('error connecting to mysql server');
+  //$conn=mysqli_connect('localhost','root','','kseblive') or die('error connecting to mysql server'); //Local Machine
+  $conn=mysqli_connect('remotemysql.com','LL0cMTBe2l','lM3fDZFQON','LL0cMTBe2l') or die('error connecting to mysql server'); //For Hosting
 ?>
