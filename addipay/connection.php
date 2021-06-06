0baa71b9afcaddd8d33032b33475de85d4d9f424
Stella M Thomas <stellamthomas2021@mca.ajce.in>
2021-06-06 14:03:55 +0530

    Hosting

--- addipay/connection.php
+++ addipay/connection.php
@@ -1,3 +1,5 @@
 <?php
-  $conn=mysqli_connect('localhost','root','','kseblive') or die('error connecting to mysql server');
+  //$conn=mysqli_connect('localhost','root','','kseblive') or die('error connecting to mysql server'); //Local Machine
+  //$conn=mysqli_connect('remotemysql.com','LL0cMTBe2l','lM3fDZFQON','LL0cMTBe2l') or die('error connecting to mysql server'); //For Hosting heroku
+  $conn=mysqli_connect('151.106.124.151','u389386359_stellamthomas','Stella1234','u389386359_kseblive') or die('error connecting to mysql server');
 ?>
