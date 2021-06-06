bdfef307e4b2894cc02f3e7d993db4b7eb671776
Stella M Thomas <stellamthomas2021@mca.ajce.in>
2021-06-06 08:00:37 +0530

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,4 +1,5 @@
 <?php
   //$conn=mysqli_connect('localhost','root','','kseblive') or die('error connecting to mysql server'); //Local Machine
-  $conn=mysqli_connect('remotemysql.com','LL0cMTBe2l','lM3fDZFQON','LL0cMTBe2l') or die('error connecting to mysql server'); //For Hosting
+  //$conn=mysqli_connect('remotemysql.com','LL0cMTBe2l','lM3fDZFQON','LL0cMTBe2l') or die('error connecting to mysql server'); //For Hosting heroku
+  $conn=mysqli_connect('151.106.124.151','u389386359_stellamthomas','sTELLA#1234','u389386359_kseblives') or die('error connecting to mysql server');
 ?>
